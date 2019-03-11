8823ba3980fec857e18d46bf80cdacf2a310c823
Daniel Schmidt <dschmidt@mesosphere.com>
2019-03-11 14:51:28 +0100

    feat: add graphql generation

--- generateTypes.js
+++ generateTypes.js
@@ -5,13 +5,56 @@ const path = require("path");
 const { execSync: exec } = require("child_process");
 const rimraf = require("rimraf");
 
-function runDTG() {
-  console.info("Running DTG");
+function getFilePaths(dir = __dirname + "/outputs/typescript") {
+  const OUTPUT_SUFFIXES = [".ts"];
+  const files = fs.readdirSync(dir);
+
+  return (
+    files
+      // remove files with wrong ending
+      .filter(path => OUTPUT_SUFFIXES.some(suffix => path.includes(suffix)))
+  );
+}
+
+// This is only needed because DTG creates .raml.ts files instead of .ts
+function getFileName(file) {
+  return file.substring(0, file.indexOf("."));
+}
+
+function removeSuffix(file) {
+  return path.basename(file, ".ts");
+}
+
+function runDTG(template) {
+  rimraf.sync(`./${template}-output/**/*`);
+  console.info("Running DTG for", template);
   const output = exec(
-    "docker run -v $(pwd)/input:/input -v $(pwd)/output:/output -v $(pwd)/graphql-template:/template dtg -s /input -t /output -o templates=/template"
+    `docker run -v $(pwd)/input:/input -v $(pwd)/outputs/${template}:/output -v $(pwd)/templates/${template}:/template dtg -s /input -t /output -o templates=/template`
   );
   console.info(`Done with DTG: '${String(output)}'`);
 }
 
-rimraf.sync("./output/**/*");
-runDTG();
+runDTG("graphql");
+runDTG("typescript");
+
+// Build Typescript export
+const files = getFilePaths();
+console.info(`Found ${files.length} files`);
+
+// Create wire up file that imports all generated files
+rimraf.sync("./outputs/typescript/index.ts");
+fs.writeFileSync(
+  "./outputs/typescript/index.ts",
+  `/** THIS FILE IS GENERATED, DO NOT EDIT IT DIRECTLY
+* THE SOURCE FOR THIS IS AT ./generateTypes.js
+**/
+${files.map(
+  file => `import * as ${getFileName(file)} from "./${removeSuffix(file)}";
+`
+)}
+
+export default {
+    ${files.map(file => `${getFileName(file)}: ${getFileName(file)}`)}
+}
+`
+);
---
+++ templates/typescript/README.md
@@ -0,0 +1,10 @@
+# Template Boilerplate
+
+This folder contains the common starting point for most of the type-generator projects.
+
+## Contents
+
+* `entrypoints.ini` : The entry point configuration
+* `fragmetns/*.stg` : Prepared snippets to provide type-agnostic templates for various operations commonly performed 
+  (ex. type serialization)
+* `structs/*.stg` : The templates for implementing the type-specific logic.
