ae900e12b0c0d2d4c650e88fcfc064b61783e0f7
Daniel Schmidt <dschmidt@mesosphere.com>
2019-04-26 11:34:17 +0200

    refactor: extract dtg image name

--- generateTypes.js
+++ generateTypes.js
@@ -2,12 +2,13 @@
 
 const { execSync: exec } = require("child_process");
 const rimraf = require("rimraf");
+const DTG_IMAGE_NAME = "dtg";
 
 function runDTG(template) {
   rimraf.sync(`./${template}-output/**/*`);
   console.info("Running DTG for", template);
   const output = exec(
-    `docker run -v $(pwd)/input:/input -v $(pwd)/outputs/${template}:/output -v $(pwd)/templates/${template}:/template dtg -s /input -t /output -o templates=/template`
+    `docker run -v $(pwd)/input:/input -v $(pwd)/outputs/${template}:/output -v $(pwd)/templates/${template}:/template ${DTG_IMAGE_NAME} -s /input -t /output -o templates=/template`
   );
   console.info(`Done with DTG: '${String(output)}'`);
 }
