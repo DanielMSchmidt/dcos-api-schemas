4ad29e1c711bb659f886f9683f629e5f3daaa1ac
Daniel Schmidt <dschmidt@mesosphere.com>
2019-04-26 11:29:54 +0200

    refactor: generate typescript to single file
    The tradeoff is that we dont need to handle imports and exports
    while loosing some code organisation

--- generateTypes.js
+++ generateTypes.js
@@ -1,29 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
 const { execSync: exec } = require("child_process");
 const rimraf = require("rimraf");
-
-function getFilePaths(dir = __dirname + "/outputs/typescript") {
-  const OUTPUT_SUFFIXES = [".ts"];
-  const files = fs.readdirSync(dir);
-
-  return (
-    files
-      // remove files with wrong ending
-      .filter(path => OUTPUT_SUFFIXES.some(suffix => path.includes(suffix)))
-  );
-}
-
-// This is only needed because DTG creates .raml.ts files instead of .ts
-function getFileName(file) {
-  return file.substring(0, file.indexOf("."));
-}
-
-function removeSuffix(file) {
-  return path.basename(file, ".ts");
-}
 
 function runDTG(template) {
   rimraf.sync(`./${template}-output/**/*`);
@@ -37,28 +15,3 @@ function runDTG(template) {
 rimraf.sync("./outputs/{typescript,graphql}/*");
 runDTG("graphql");
 runDTG("typescript");
-
-// Build Typescript export
-const files = getFilePaths();
-console.info(`Found ${files.length} files`);
-
-// Create wire up file that imports all generated files
-fs.writeFileSync(
-  "./outputs/typescript/index.ts",
-  `/** THIS FILE IS GENERATED, DO NOT EDIT IT DIRECTLY
-* THE SOURCE FOR THIS IS AT ./generateTypes.js
-**/
-${files
-  .map(
-    file => `import * as ${getFileName(file)} from "./${removeSuffix(file)}";
-`
-  )
-  .join("")}
-
-export default {
-    ${files
-      .map(file => `${getFileName(file)}: ${getFileName(file)}`)
-      .join(",\n")}
-}
-`
-);
